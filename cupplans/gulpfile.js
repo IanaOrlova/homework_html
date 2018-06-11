e85d295b5bd86b2ba9b6d6ddfb61fba2be79373e
Yana <38989612+IanaOrlova@users.noreply.github.com>
2018-06-11 17:16:36 +0300

    gulp

--- cupplans/gulpfile.js
+++ cupplans/gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('watch', function() {
 });
 
 gulp.task('style', function() {
-    return gulp.src('sass/*.scss', {style : 'expended'})
+    return gulp.src('sass/main.scss', {style : 'expended'})
         .pipe(sass({includePaths: ['sass/**']}))
         .pipe(rename({suffix: '.min'}))
         .pipe(minify())
